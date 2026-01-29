a51d0be7297350e0bc10158c57741271548ba68f
RaoHaiderAliJohar99136 <raohaiderjohar99@gmail.com>
2026-01-29 19:48:32 +0500

    Add files via upload

---
+++ component.py
@@ -0,0 +1,54 @@
+import streamlit as st
+st.title("Streamlit Tutorial App")
+st.write("This is my new app")
+button1 = st.button("Click Me")
+if button1:
+    st.write("This is some text.")
+
+st.header("Start of the Checkbox Section")
+like = st.checkbox("Do you like this app?")
+button2 = st.button("Submit")
+if button2:
+    if like:
+        st.write("Thanks. I like it too.")
+    else:
+        st.write("I'm sorry. You have bad tastes.")
+
+st.header("Start of the Radio Button Section")
+animal = st.radio("What animal is your favorite?", ("Lion", "Tiger", "Bear"))
+button3 = st.button("Submit Animal")
+if button3:
+    st.write(animal)
+    if animal == "Lion":
+        st.write("ROAR!")
+
+st.header("Start of the Selectbox Section")
+animal2 = st.selectbox("What animal is your favorite?", ("Lion", "Tiger", "Bear"))
+button4 = st.button("Submit Animal 2")
+if button4:
+    st.write(animal2)
+    if animal2 == "Lion":
+        st.write("ROAR!")
+
+
+st.header("Start of the Multiselect Section")
+options = st.multiselect("What animals do you like?",
+["Lion", "Tiger", "Bear"])
+button5 = st.button("Print Animals")
+if button5:
+    st.write(options)
+
+
+st.header("Start of the Slider Section")
+epochs_num = st.slider("How many epochs?", 1,100, 10)
+if st.button("Slider Button"):
+    st.write(epochs_num)
+
+st.header("Start of the Text Input Section")
+user_text = st.text_input("What's your favorite movie?", "National TV ! ")
+if st.button("Text Button"):
+    st.write(int(user_text))
+
+user_num = st.number_input("What's your favorite number?")
+if st.button("Number Button"):
+    st.write(user_num)
